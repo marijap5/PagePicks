644f39f6981c43d75803a3fb73a930c3904759bb
Andrej Shumkovski <85301264+Shume00@users.noreply.github.com>
2023-12-06 19:19:59 +0100

    Initial commit

---
+++ src/main/java/com/timskiproekt/pagepicks/PagePicksApplication.java
@@ -0,0 +1,13 @@
+package com.timskiproekt.pagepicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagePicksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagePicksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/timskiproekt/pagepicks/PagePicksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timskiproekt.pagepicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagePicksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
